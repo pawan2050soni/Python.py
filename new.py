0bcfb8172c4a7668e36f9f6e8d3a3397432fba17
Pawan <pawan2050soni@gmail.com>
2020-05-23 18:54:01 +0530

    New line added-for software.

--- new.py
+++ new.py
@@ -1,4 +1,5 @@
 print('Wowsome time, Vineet Kapoor here!')
 #New line added
 print('Wondeful day started coding in pycharm saving same file project in Github')
-
+#lets work to made new code for software.
+print('New software started the coding')
